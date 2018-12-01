224a0e41f78bf8992a64b100c486b3a7e7f6e13f
Alexander Amling <aja7073@rit.edu>
2018-12-01 00:17:23 -0500

    Functioning Raycasting and Rendering. Made corrections to classes.js and wrote basic main.js
    Got functioning rendering and raycasting.
    
    Still needs perspective adjustment.
    
    corrected indexing in classes.js to index into the array of points in each ray, rather than attempt to index into the ray itself
    
    Added a map Setup function for building a level. Added placeholder map creation

--- js/classes.js
+++ js/classes.js
@@ -34,13 +34,15 @@ Player.RayCast(){
 	inspect each step returning whe you hit a wall or edge of map
 }
 */
+
 const PI = Math.PI;
 
 // holds the player position and direction
 class Player{
-	constructor(x=0,y=0, direction){
-		this.position = new Point(x,y);
+	constructor(x=0,y=0, direction = 0, POV = 90){
+		this.position = new Vector2(x,y);
 		this.direction = direction;
+		this.POV = POV;
 	}
 }
 
@@ -61,16 +63,34 @@ Player.prototype.Move = function(distance, map) {
 
 // class for the rendering of the scene
 class Camera{
-	constructor(map, scene, resolution){
+	constructor(map, scene, sceneWidth, sceneHeight, resolution){
 		this.map = map;
 		this.scene = scene;
 		this.resolution = resolution;
+		this.sceneWidth = sceneWidth;
+		this.sceneHeight = sceneHeight;
+		this.wallScale = 250;
+		this.walls = this.GetWalls(sceneWidth, resolution);
 	}
 }
+
+Camera.prototype.GetWalls = function(sceneWidth, resolution) {
+	let wallWidth = sceneWidth / resolution;
+	let walls = [resolution];
+	for (let i = 0; i < resolution; i++){
+		walls[i] = new PIXI.Graphics();
+		walls[i].beginFill(0x888888);
+		walls[i].drawRect(0,0,wallWidth,1);
+		walls[i].endFill();
+		this.scene.addChild(walls[i]);
+	}
+	return walls;
+};
 
 Camera.prototype.Update = function(player) {
 	// draw background
 	// draw walls
+	this.DrawWalls(player);
 };
 
 // method to handle drawing the background (floor and/or sky)
@@ -82,15 +102,27 @@ Camera.prototype.DrawBackground = function() {
 Camera.prototype.DrawWalls = function(player) {
 	// divide player FOV (default 90?) into desired resolution
 	// for each division
+	for (let i = 0; i < this.resolution; i++){
+		let angle = player.direction - (player.POV / 2);
+		angle = angle + ((i/this.resolution) * player.POV);
 		// create new ray
-		Ray ray = new Ray(angle);
+		let ray = new Ray(angle);
 		// give the ray the player position as the starting point
 		ray.points.push(player.position);
 		// raycast
-		map.raycast(ray);
-		// create new pixi shape to retrieve from shape pool
+		this.map.RayCast(ray);
 		// change position to the nessicary position on screen
-		// scale vertically depending on the distance - heigh = cos(FOVAngle) * distance;
+		this.walls[i].x = this.sceneWidth * (i/this.resolution);
+		// scale vertically depending on the distance 
+		let lastPoint = ray.points[ray.points.length - 1];
+		let distance = (lastPoint.x - player.position.x) * (lastPoint.x - player.position.x);
+		distance += (lastPoint.y - player.position.y) * (lastPoint.y - player.position.y);
+		distance = Math.sqrt(distance) * Math.cos(player.direction);
+		this.walls[i].height =  this.wallScale / distance;
+		this.walls[i].y = (this.sceneHeight * .5) - (this.walls[i].height/2);
+		//debugger;
+	}
+	//debugger;
 };
 
 // class for the state info of the scene and raycasting through the scene
@@ -98,19 +130,20 @@ class Map{
 	constructor(size){
 		this.size = size;
 		this.wallGrid = new Uint8Array(size * size); // array to hold the map values
+		this.Setup(); // set up the walls
 	}
 }
 
 // method for checking the value of the map a given point
-Map.prototype.Inspect = function(point) {
+Map.prototype.Inspect = function(ray) {
 	if (point.x % 1 == 0){ // check the spaces on either side horizontally
-		if(wallGrid[point.y * size + point.x] > 0 || wallGrid[point.y * size + point.x - 1] > 0 ){
+		if(this.wallGrid[Math.floor(point.y) * this.size + Math.floor(point.x)] > 0 || this.wallGrid[Math.floor(point.y) * this.size + Math.floor(point.x - 1)] > 0 ){
 			return true;
 		} else {
 			return false;
 		}
 	} else { // check the spaces above and below
-		if(wallGrid[point.y * size + point.x] > 0 || wallGrid[(point.y - 1) * size + point.x] > 0 ){
+		if(this.wallGrid[Math.floor(point.y) * this.size + Math.floor(point.x)] > 0 || this.wallGrid[(Math.floor(point.y - 1)) * this.size + Math.floor(point.x)] > 0 ){
 			return true;
 		} else {
 			return false;
@@ -122,8 +155,8 @@ class Ray{
 	constructor(angle){
 		this.points = [];
 		this.angle = angle;
-		this.angleSin = Math.sin(angle);
-		this.angleCos = Math.cos(angle);
+		this.angleSin = Math.sin(angle * (PI/180));
+		this.angleCos = Math.cos(angle * (PI/180));
 		this.slope = this.angleSin/this.angleCos;
 		this.right = this.angleCos > 0;
 		this.up = this.angleSin > 0;
@@ -144,42 +177,64 @@ class Vector2{
 */
 
 Map.prototype.RayCast = function(ray) {
+	let newPoint;
 
-	// create next point in a ray
-	let newPoint = new Vector2(0,0);
+	do{
+		// create next point in a ray
+		newPoint = new Vector2(0,0);
 
-	// determine which axis the ray will hit next
-	let DistToX = 0;
-	let DistToY = 0;
-	if(ray.right){
-		DistToX = 1 - (ray[length-1].x % 1); // get the distance from the last point to the right boundary
-	} else {
-		DistToX = -1 + (ray[length-1].x % 1); // get the distance from the last point to the left boundary
+		// determine which axis the ray will hit next
+		let DistToX = 0;
+		let DistToY = 0;
+
+		//console.log("rev Point: " + ray.points[ray.points.length-1].x + " | " + ray.points[ray.points.length-1].y);
+
+
+		if(ray.right){
+			DistToX = 1 - (ray.points[ray.points.length-1].x % 1); // get the distance from the last point to the right boundary
+		} else {
+			DistToX = -1 + (ray.points[ray.points.length-1].x % 1); // get the distance from the last point to the left boundary
+		}
+
+		if(ray.up){
+			DistToY = 1 - (ray.points[ray.points.length-1].y % 1); // get the distance from the last point to the top boundary
+		} else {
+			DistToY = -1 + (ray.points[ray.points.length-1].y % 1); // get the distance from the last point to the bottom boundary
+		}
+
+		//console.log("Dist: " + DistToX + " | " + DistToY);
+		//console.log("Slope: " + ray.slope);
+
+		if(Math.abs(DistToY) > (Math.abs(DistToX) * ray.slope)){
+			// if the horizontal boundary is closer
+			//console.log("horizontal");
+			newPoint.x =  Math.round(ray.points[ray.points.length - 1].x + DistToX);
+			newPoint.y = ray.points[ray.points.length - 1].y + (DistToX * ray.slope);
+		} else {
+			// if the vertical boundary is closer
+			//console.log("vertical");
+			newPoint.x =  ray.points[ray.points.length - 1].x + (DistToY / ray.slope);
+			newPoint.y = Math.round(ray.points[ray.points.length - 1].y + DistToY);
+		}
+		//debugger;
+		// add point to ray
+		ray.points.push(newPoint);
+
+	} while (!this.Inspect(ray)) // exit loop when the ray hits a wall
+};
+
+Map.prototype.Setup = function(size) {
+	// populated the map wallGrid
+	// placeholder for randomized/prodecural loading
+	for (let i = 0; i < this.size * this.size; i++) {
+		if (i / this.size == 0 || i / this.size == this.size - 1){
+			this.wallGrid[i] = 1;
+		}
+		else if (i % this.size == 0 || i % this.size == this.size - 1){
+			this.wallGrid[i] = 1;
+		}
+		else{
+			this.wallGrid[i] = 0;
+		}
 	}
-
-	if(ray.up){
-		DistToY = 1 - (ray[length-1].y % 1); // get the distance from the last point to the top boundary
-	} else {
-		DistToY = -1 + (ray[length-1].y % 1); // get the distance from the last point to the bottom boundary
-	}
-
-	if((DistToY\DistToX) > ray.slope){
-		// if the vertical boundary is closer
-		newPoint.x =  ray[ray.length - 1].x + DistToX;
-		newPoint.y = ray[ray.length - 1].y + (DistToX * ray.slope);
-	} else {
-		// if horizontal boundary is closer
-		newPoint.x =  ray[ray.length - 1].x + (DistToY / ray.slope);
-		newPoint.y = ray[ray.length - 1].y + DistToY;
-	}
-
-	// add point to ray
-	ray.points.push(newPoint);
-
-	// inspect new point
-	if (inspect(newPoint)){
-		return; // exit loop when the ray hits a wall
-	}
-
-	RayCast(ray);
 };
---
+++ js/main.js
@@ -0,0 +1,75 @@
+
+"use strict";
+const app = new PIXI.Application(480,320);
+document.body.appendChild(app.view);
+
+const sceneWidth = app.view.width;
+const sceneHeight = app.view.height;
+const resolution = 24;
+
+let startScene;
+let gameScene;
+let gameOverScene;
+
+let player;
+let map;
+let cam;
+
+let playing = true;
+
+Setup();
+
+function Setup(){
+	// #1 - Create the `start` scene
+	startScene = new PIXI.Container();
+	startScene.visible = false;
+	app.stage.addChild(startScene);
+
+	// #2 - Create the main `game` scene and make it invisible
+	gameScene = new PIXI.Container();
+	//gameScene.visible = false;
+	app.stage.addChild(gameScene);
+
+	// #3 - Create the `gameOver` scene and make it invisible
+	gameOverScene = new PIXI.Container();
+	gameOverScene.visible = false;
+	app.stage.addChild(gameOverScene);
+
+	// #4 - Labels for the scenes
+
+	// #5 - Create Player
+	player = new Player();
+	player.position = new Vector2(5,5);
+
+	// #6 - Create Map
+	map = new Map(10);
+
+	// #7 - Create Camera
+	cam = new Camera(map, gameScene, sceneWidth, sceneHeight, resolution);
+
+	// #8 - Start Game Loop
+	app.ticker.add(GameLoop);
+}
+
+function GameLoop(){
+	if (!playing) return;
+
+	// update player
+	//player.Update();
+
+	// update camera
+	debugger;
+	cam.Update(player);
+	// check if game has ended
+}
+
+/* TODO:
+input
+player movement
+player collisions
+map loading
+draw floor/background
+tuning of the camera
+start screen
+end screen
+*/
